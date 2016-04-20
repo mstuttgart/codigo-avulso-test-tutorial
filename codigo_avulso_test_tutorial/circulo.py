f3edb4123ab6e50efc7571dbc799171200fa5eef
mstuttgart <michellstut@gmail.com>
2016-04-19 23:57:59 -0300

    Fix pep8

--- codigo_avulso_test_tutorial/circulo.py
+++ codigo_avulso_test_tutorial/circulo.py
@@ -2,10 +2,11 @@
 import math
 from figura_geometrica import FiguraGeometrica
 
+
 class Circulo(FiguraGeometrica):
 
     def __init__(self):
-      self.raio = 0
+        self.raio = 0
 
     # Retorna a area do circulo
     def get_area(self):
@@ -14,4 +15,3 @@ class Circulo(FiguraGeometrica):
     # Retorna o perimetro do circulo
     def get_perimetro(self):
         return 2 * math.pi * self.raio
-
